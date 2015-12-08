9c53521f4ffef0701e7a66f597c66460805888d7
Paulo Ricardo Araújo <pauloricardo722@gmail.com>
2015-12-07 23:12:03 -0300

    Commit inicial no repositorio pessoal

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -0,0 +1,89 @@
+package com.example.android.sunshine.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class DetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+
+//        Intent intent = getIntent();
+//        String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+//        Bundle bundle = new Bundle();
+//        bundle.putString("forecast", forecast);
+//
+//        PlaceholderFragment fragment = new PlaceholderFragment();
+//        fragment.setArguments(bundle);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(getApplication(), "Settings", Toast.LENGTH_SHORT).show();
+            Intent settingsActivity = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(settingsActivity);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            TextView detail = (TextView)rootView.findViewById(R.id.detail_text_view);
+
+            Intent intent = getActivity().getIntent();
+            detail.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+
+            return rootView;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
@@ -0,0 +1,27 @@
+package com.example.android.sunshine.app;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WeatherDataParser {
+
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+        // TODO: add parsing code here
+        JSONObject json = new JSONObject(weatherJsonStr);
+        JSONArray list = json.getJSONArray("list");
+        JSONObject day = list.getJSONObject(dayIndex);
+        JSONObject temp = day.getJSONObject("temp");
+        Double maxTemp = (Double)temp.get("max");
+        return maxTemp;
+    }
+
+}
+
